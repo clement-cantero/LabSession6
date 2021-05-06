712a4d5d8c3b04b115416c009a1e2f38e77ed683
clement-cantero <clement.cantero@etu.umontpellier.fr>
2021-05-06 10:12:32 +0000

    first commit

---
+++ src/graph/TestGraph.java
@@ -0,0 +1,12 @@
+public class TestGraph{
+
+    public static void main(String[] args){
+        Vertex v1 = new Vertex(9, "Test", "red");
+        Vertex v2 = new Vertex(10, "Test", "red");
+        Edge e = new UndirectedEdge(2, "red", new Vertex[]{v1, v2}, 4.5);
+
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(e);
+    }
+}
